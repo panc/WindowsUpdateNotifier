3091d8d62b321725dd20f5d4362f94077fc9f249
panc <christoph.pangerl@gmail.com>
2012-12-04 21:31:10 +0100

    Hide popup after 20 seconds.

--- src/WindowsUpdateNotifier.Desktop/Model/PopupViewModel.cs
+++ src/WindowsUpdateNotifier.Desktop/Model/PopupViewModel.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace WindowsUpdateNotifier.Desktop
 {
     public class PopupViewModel
     {
-        public PopupViewModel(string title, string message, Action onCloseClicked, IApplication application)
+        public PopupViewModel(string title, string message, Action onCloseCallback, IApplication application)
         {
             Title = title;
             Message = message;
-            OnCloseCommand = new SimpleCommand(onCloseClicked);
+            OnCloseCommand = new SimpleCommand(onCloseCallback);
             OnOpenSettingsViewCommand = new SimpleCommand(application.OpenSettingsView);
             OnOpenWindowsUpdateControlPanelCommand= new SimpleCommand(application.OpenWindowsUpdateControlPanel);
+
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(20) };
+            timer.Tick += (s, e) =>
+            {
+                onCloseCallback();
+                timer.Stop();
+            };
+
+            timer.Start();
         }
 
         public ICommand OnCloseCommand { get; set; }
