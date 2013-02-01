d22a2853e37be4158796c90ea533fa31061fa848
panc <christoph.pangerl@gmail.com>
2013-02-01 07:49:17 +0100

    Updated version to 1.0 as this version is verified as a stable version and it isn't in test state anymore.

--- src/WindowsUpdateNotifier/Properties/AssemblyInfo.cs
+++ src/WindowsUpdateNotifier/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("0.6.2.1")]
+[assembly: AssemblyVersion("1.0.0.6")]
