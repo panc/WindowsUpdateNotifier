cdaeb85f19cde86f575fb05fcf89423c893f69d0
panc <christoph.pangerl@gmail.com>
2013-02-10 11:48:00 +0100

    Added UAC helper to check whether adminrights are available.

---
+++ src/WindowsUpdateNotifier/Helper/UacHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Principal;
+
+namespace WindowsUpdateNotifier
+{
+    public class UacHelper
+    {
+        public static bool IsRunningAsAdmin()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
