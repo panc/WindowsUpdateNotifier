54af3bc679ba51449e2310174a35cdde09b10b6a
panc <christoph.pangerl@gmail.com>
2013-03-01 23:37:06 +0100

    Added msbuild script to create a release.

---
+++ _build_debug.cmd
@@ -0,0 +1,3 @@
+@ECHO OFF
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe WindowsUpdateNotifier.msbuild /t:BuildDebug
+pause
--- src/WindowsUpdateNotifier/Properties/AssemblyInfo.cs
+++ src/WindowsUpdateNotifier/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
@@ -15,5 +15,5 @@ using System.Windows;
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.1.0.7")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0.8")]
+[assembly: AssemblyFileVersion("1.2.0")]
