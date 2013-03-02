a1c38b3175d66361821e913d30149b66fc23a751
panc <christoph.pangerl@gmail.com>
2013-03-02 13:22:28 +0100

    Adjusted RssVersionReader to work with the new version naming.

--- src/WindowsUpdateNotifier/Versioning/RssVersionItem.cs
+++ src/WindowsUpdateNotifier/Versioning/RssVersionItem.cs
@@ -4,7 +4,8 @@ namespace WindowsUpdateNotifier
 {
     public class RssVersionItem
     {
-        private const string RSS_TITLE = "Windows (8) Update Notifier - V";
+        private const string RSS_TITLE_OLD = "Windows (8) Update Notifier - V";
+        private const string RSS_TITLE = "Windows Update Notifier - V";
 
         public RssVersionItem(string title, string link, DateTime date)
         {
@@ -25,8 +26,16 @@ namespace WindowsUpdateNotifier
 
         private string _GetVersion(string title)
         {
-            var index = title.IndexOf(RSS_TITLE, 0, StringComparison.Ordinal);
-            var version = title.Substring(index + RSS_TITLE.Length);
+            var index = title.IndexOf(RSS_TITLE_OLD, 0, StringComparison.Ordinal);
+            var length = RSS_TITLE_OLD.Length;
+
+            if (index < 0)
+            {
+                index = title.IndexOf(RSS_TITLE, 0, StringComparison.Ordinal);
+                length = RSS_TITLE.Length;
+            }
+
+            var version = title.Substring(index + length);
 
             return version.Substring(0, version.IndexOf(' '));
         }
