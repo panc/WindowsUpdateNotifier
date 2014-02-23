b840a44b617075d70efb43eb093eb5f460fdff63
panc <christoph.pangerl@gmail.com>
2014-02-23 18:00:45 +0100

    Restructured views and viewmodels.

---
+++ src/WindowsUpdateNotifier/Gui/Views/Settings/SettingsView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace WindowsUpdateNotifier
+{
+    public partial class SettingsView : Window
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
