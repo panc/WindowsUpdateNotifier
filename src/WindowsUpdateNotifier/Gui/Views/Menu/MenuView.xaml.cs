ac19c3b50cd62b8649c1525866f04ba60091a65f
panc <christoph.pangerl@gmail.com>
2014-03-23 11:47:19 +0100

    Correct menu location.

--- src/WindowsUpdateNotifier/Gui/Views/Menu/MenuView.xaml.cs
+++ src/WindowsUpdateNotifier/Gui/Views/Menu/MenuView.xaml.cs
@@ -5,6 +5,8 @@ namespace WindowsUpdateNotifier
 {
     public partial class MenuView
     {
+        private const int OFFSET = 10;
+
         public MenuView()
         {
             InitializeComponent();
@@ -13,11 +15,43 @@ namespace WindowsUpdateNotifier
         private void _OnWindowLoaded(object sender, RoutedEventArgs e)
         {
             var workingArea = SystemParameters.WorkArea;
-            Left = CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft
-                ? workingArea.Left + 10
-                : workingArea.Width + workingArea.Left - Width - 10;
+            var taskBarLocation = _GetTaskBarLocation(workingArea);
+            var isLeftToRight = CultureInfo.CurrentUICulture.TextInfo.IsRightToLeft;
 
-            Top = workingArea.Height + workingArea.Top - ActualHeight - 10;
+            if (taskBarLocation == TaskBarLocation.Top || taskBarLocation == TaskBarLocation.Bottom)
+            {
+                Left = (isLeftToRight)
+                   ? workingArea.Left + OFFSET
+                   : workingArea.Width + workingArea.Left - Width - OFFSET;
+                
+                Top = (taskBarLocation == TaskBarLocation.Bottom)
+                    ? workingArea.Height - ActualHeight - OFFSET
+                    : workingArea.Top + OFFSET;
+            }
+            else
+            {
+                Left = (taskBarLocation == TaskBarLocation.Right)
+                    ? workingArea.Width - ActualWidth - OFFSET
+                    : workingArea.Left + OFFSET;
+
+                Top = (isLeftToRight)
+                    ? workingArea.Top + OFFSET
+                    : workingArea.Height + workingArea.Top - ActualHeight - OFFSET;
+            }
         }
+        
+        private TaskBarLocation _GetTaskBarLocation(Rect workingArea)
+        {
+            if (workingArea.Left > 0)
+                return TaskBarLocation.Left;
+            if (workingArea.Top > 0)
+                return TaskBarLocation.Top;
+            if (workingArea.Left == 0 && workingArea.Width < SystemParameters.PrimaryScreenWidth)
+                return TaskBarLocation.Right;
+            
+            return TaskBarLocation.Bottom;
+        }
+
+        private enum TaskBarLocation { Top, Bottom, Left, Right }
     }
 }
