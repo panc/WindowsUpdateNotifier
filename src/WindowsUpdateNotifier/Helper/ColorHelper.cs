0045b1be5555fe0b9730a6c97f1f6402532f28eb
Christoph Pangerl <christoph.pangerl@gmail.com>
2015-06-17 23:55:02 +0200

    Fixed issue which returns the wrong color when retrieving the windows theme color.

--- src/WindowsUpdateNotifier/Helper/ColorHelper.cs
+++ src/WindowsUpdateNotifier/Helper/ColorHelper.cs
@@ -6,53 +6,69 @@ namespace WindowsUpdateNotifier
 {
     public static class ColorHelper
     {
+        private static readonly Color INVALID_COLOR = Color.FromArgb(255, 255, 0, 255);
+
         public static Color GetWindowsThemeBackgroundColor()
+        {
+            var color = _GetWindowsThemeBackgroundColor();
+
+            // Somtimes it happens that the first attemt to get the systems theme color 
+            // returns an invalid color (always RGBA(255, 0, 255, 255)).
+            // If this happens we just try it again
+
+            if (color == INVALID_COLOR)
+                color = _GetWindowsThemeBackgroundColor();
+
+            return color;
+        }
+
+        private static Color _GetWindowsThemeBackgroundColor()
         {
             if (UacHelper.IsRunningOnWindows7())
                 return Color.FromArgb(255, 35, 38, 39);
 
-            var colourset = GetImmersiveUserColorSetPreference(false, false);
-            var pElementName = Marshal.StringToHGlobalUni("ImmersiveStartBackground");
-            Marshal.FreeCoTaskMem(pElementName);
+            var colorSet = GetImmersiveUserColorSetPreference(false, false);
+            var elementName = Marshal.StringToHGlobalUni("ImmersiveStartBackground");
+            Marshal.FreeCoTaskMem(elementName);
 
-            var type = GetImmersiveColorTypeFromName(pElementName);
-            var colourdword = GetImmersiveColorFromColorSetEx((uint)colourset, type, false, 0);
+            var type = GetImmersiveColorTypeFromName(elementName);
+            var colorDword = GetImmersiveColorFromColorSetEx((uint) colorSet, type, false, 0);
 
-            var colourbytes = new byte[4];
-            colourbytes[0] = (byte)((0xFF000000 & colourdword) >> 24); // A
-            colourbytes[1] = (byte)((0x00FF0000 & colourdword) >> 16); // B
-            colourbytes[2] = (byte)((0x0000FF00 & colourdword) >> 8); // G
-            colourbytes[3] = (byte)(0x000000FF & colourdword); // R
+            var colorBytes = new byte[4];
+            colorBytes[0] = (byte) ((0xFF000000 & colorDword) >> 24); // A
+            colorBytes[1] = (byte) ((0x00FF0000 & colorDword) >> 16); // B
+            colorBytes[2] = (byte) ((0x0000FF00 & colorDword) >> 8); // G
+            colorBytes[3] = (byte) ((0x000000FF & colorDword) >> 0); // R
 
-            return Color.FromArgb(colourbytes[0], colourbytes[3], colourbytes[2], colourbytes[1]);
+            return Color.FromArgb(colorBytes[0], colorBytes[3], colorBytes[2], colorBytes[1]);
         }
 
         /// <summary>
         /// Retrieves an immersive colour from the specified colour set.
         /// </summary>
-        /// <param name="dwImmersiveColorSet">Colour set index.</param>
-        /// <param name="dwImmersiveColorType">The colour type. Use <see cref="GetImmersiveColorTypeFromName"/> to get the type from an element name.</param>
-        /// <param name="bIgnoreHighContrast">Set this to true to return colours from the current colour set, even if a high contrast theme is being used.</param>
-        /// <param name="dwHighContrastCacheMode">Set this to 1 to force UxTheme to check whether high contrast mode is enabled. If this is set to 0, UxTheme will only perform this check if high contrast mode is currently disabled.</param>
+        /// <param name="immersiveColorSet">Colour set index.</param>
+        /// <param name="immersiveColorType">The colour type. Use <see cref="GetImmersiveColorTypeFromName"/> to get the type from an element name.</param>
+        /// <param name="ignoreHighContrast">Set this to true to return colours from the current colour set, even if a high contrast theme is being used.</param>
+        /// <param name="highContrastCacheMode">Set this to 1 to force UxTheme to check whether high contrast mode is enabled. If this is set to 0, UxTheme will only perform this check if high contrast mode is currently disabled.</param>
         /// <returns>Returns a colour (0xAABBGGRR).</returns>
         [DllImport("uxtheme.dll", EntryPoint = "#95")]
-        private static extern uint GetImmersiveColorFromColorSetEx(uint dwImmersiveColorSet, uint dwImmersiveColorType, bool bIgnoreHighContrast, uint dwHighContrastCacheMode);
+        private static extern uint GetImmersiveColorFromColorSetEx(uint immersiveColorSet, uint immersiveColorType, bool ignoreHighContrast, uint highContrastCacheMode);
 
         /// <summary>
         /// Gets the user's colour set preference (or default colour set if the user isn't allowed to modify this setting according to group policy).
         /// </summary>
-        /// <param name="bForceCheckRegistry">Forces update from registry (HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent\ColorSet_Version3).</param>
-        /// <param name="bSkipCheckOnFail">Skip second check if first result is -1.</param>
+        /// <param name="forceCheckRegistry">Forces update from registry (HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Accent\ColorSet_Version3).</param>
+        /// <param name="skipCheckOnFail">Skip second check if first result is -1.</param>
         /// <returns>User colour set preference.</returns>
         [DllImport("uxtheme.dll", EntryPoint = "#98")]
-        private static extern int GetImmersiveUserColorSetPreference(bool bForceCheckRegistry, bool bSkipCheckOnFail);
+        private static extern int GetImmersiveUserColorSetPreference(bool forceCheckRegistry, bool skipCheckOnFail);
 
         /// <summary>
         /// Retrieves an immersive colour type given its name.
         /// </summary>
-        /// <param name="pName">Pointer to a string containing the name preprended with 9 characters (e.g. "Immersive" + name).</param>
+        /// <param name="name">Pointer to a string containing the name preprended with 9 characters (e.g. "Immersive" + name).</param>
         /// <returns>Colour type.</returns>
         [DllImport("uxtheme.dll", EntryPoint = "#96")]
-        private static extern uint GetImmersiveColorTypeFromName(IntPtr pName); 
+        private static extern uint GetImmersiveColorTypeFromName(IntPtr name); 
     }
 }
